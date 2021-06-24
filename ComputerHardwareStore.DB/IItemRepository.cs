2fbeb7abce9fe9dafaabeecd784087ea9cc32980
Damup1988 <damup1988@gmail.com>
2021-06-25 00:31:27 +0300

    Item class has been described Repository interface has been created

---
+++ ComputerHardwareStore.DB/IItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ComputerHardwareStore.Domain;
+
+namespace ComputerHardwareStore.DB
+{
+    public interface IItemRepository
+    {
+        IEnumerable<Item> GetAllItems();
+        Item GetItemById(Guid id);
+        void CreateItem(Item item);
+        void UpdateItem(Item item);
+        void DeleteItem(Item item);
+    }
+}
---
+++ ComputerHardwareStore.Domain/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComputerHardwareStore.Domain
+{
+    public class Item
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Vendor { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Type { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public bool IsAvailable => Quantity > 0;
+        [Required]
+        [MaxLength(100)]
+        public string ShortDescription { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
